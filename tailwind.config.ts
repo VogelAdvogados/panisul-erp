20ffd5a8317e1399d6f408d6aa0e8e99f160462f
mauricius vogel <spriteco@gmail.com>
2025-08-21 18:11:30 +0000

    VOCÊ PRECISA FAZER UMA ANÁLISE MINUSCIOSA EM TODO O CÓDIGO PARA EVITAR E

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['var(--font-sans)', 'sans-serif'],
+        headline: ['"PT Sans"', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
